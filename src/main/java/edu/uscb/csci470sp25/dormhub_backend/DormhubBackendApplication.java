b31bf4e1948b14655ea7d3d9ea38822075dfc45d
Keaton LaBorde <klaborde@email.uscb.edu>
2025-03-30 14:20:22 -0400

    initial commit

---
+++ src/main/java/edu/uscb/csci470sp25/dormhub_backend/DormhubBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.uscb.csci470sp25.dormhub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormhubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormhubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uscb/csci470sp25/dormhub_backend/DormhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uscb.csci470sp25.dormhub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormhubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
