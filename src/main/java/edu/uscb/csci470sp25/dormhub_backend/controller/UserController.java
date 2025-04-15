d54703228b63a9c1e22ca3068896cb19ed07acec
Keaton LaBorde <klaborde@email.uscb.edu>
2025-04-15 14:20:41 -0400

    Normalized database and fixed usertasks and users

--- src/main/java/edu/uscb/csci470sp25/dormhub_backend/controller/UserController.java
+++ src/main/java/edu/uscb/csci470sp25/dormhub_backend/controller/UserController.java
@@ -5,9 +5,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import edu.uscb.csci470sp25.dormhub_backend.exception.UserNotFoundException;
-import edu.uscb.csci470sp25.dormhub_backend.model.AppUser;
 import edu.uscb.csci470sp25.dormhub_backend.model.User;
-import edu.uscb.csci470sp25.dormhub_backend.repository.AppUserRepository;
 import edu.uscb.csci470sp25.dormhub_backend.repository.UserRepository;
  
 @RestController
@@ -15,20 +13,17 @@ public class UserController {
  
     @Autowired
     private UserRepository userRepository;
-
-    @Autowired
-    private AppUserRepository appUserRepository;
  
     // Create a new user
     @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping("/user")
     public User newUser(@RequestBody User newUser) {
-        // Fetch the AppUser by ID (from the app_users table)
-        AppUser appUser = appUserRepository.findById(newUser.getAppUser().getId())
-                .orElseThrow(() -> new UserNotFoundException(newUser.getAppUser().getId()));
-        // Set the appUser in the newUser object
-        newUser.setAppUser(appUser);
-        // Save the new user
+        if (userRepository.existsByUsername(newUser.getUsername())) {
+            throw new RuntimeException("Username is already taken");
+        }
+        if (userRepository.existsByEmail(newUser.getEmail())) {
+            throw new RuntimeException("Email is already in use");
+        }
         return userRepository.save(newUser);
     }
  
@@ -57,6 +52,9 @@ public class UserController {
                 .map(user -> {
                     user.setUsername(newUser.getUsername());
                     user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    user.setPassword(newUser.getPassword());
+                    user.setRole(newUser.getRole());
                     return userRepository.save(user);
                 }).orElseThrow(() -> new UserNotFoundException(id));
 
