2830a8c7f4d9d9000f14c956d702b4568917b0a1
Keaton LaBorde <klaborde@email.uscb.edu>
2025-04-18 12:52:45 -0400

    Admin Users are only able to see other users with role PRIVILEGED_USER

--- src/main/java/edu/uscb/csci470sp25/dormhub_backend/repository/UserRepository.java
+++ src/main/java/edu/uscb/csci470sp25/dormhub_backend/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package edu.uscb.csci470sp25.dormhub_backend.repository;
 
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
+    List<User> findByRoleOrderByIdAsc(String role);
+
 }
